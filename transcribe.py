4c4264967d09ed4791206249641d9a9a2a4d3fd4
Rakheeb <rakheebshaikh906@gmail.com>
2026-07-31 19:33:50 +0530

    update

--- transcribe.py
+++ transcribe.py
@@ -1,4 +1,5 @@
 import sys
+import time
 from faster_whisper import WhisperModel
 
 print("WHISPER_LOADING", flush=True)
@@ -14,7 +15,7 @@ print("WHISPER_READY", flush=True)
 
 initial_prompt = (
     "This is a Jarvis voice assistant. "
-    "Commands include: Call Abbu, Call Ashu, Call Sahil, "
+    "Commands include: Call Abbu, Call Ashu, Call mummy, "
     "open YouTube on my phone, open WhatsApp on my phone, "
     "open Instagram on my phone, open Camera on my phone, "
     "open Gallery on my phone."
@@ -30,6 +31,11 @@ for line in sys.stdin:
 
     try:
 
+        print("--------------------------------", flush=True)
+        print("START_TRANSCRIBE", flush=True)
+
+        start = time.time()
+
         segments, info = model.transcribe(
             audio_path,
             beam_size=1,
@@ -38,12 +44,30 @@ for line in sys.stdin:
             vad_filter=True
         )
 
+        after_transcribe = time.time()
+
+        print(
+            f"TRANSCRIBE_TIME: {after_transcribe - start:.3f} sec",
+            flush=True
+        )
+
         text = " ".join(
             segment.text.strip()
             for segment in segments
         ).strip()
 
-        # Electron ko predictable single-line result
+        after_join = time.time()
+
+        print(
+            f"JOIN_TIME: {after_join - after_transcribe:.3f} sec",
+            flush=True
+        )
+
+        print(
+            f"FINAL_TEXT: {text}",
+            flush=True
+        )
+
         print(
             "TRANSCRIPT:" + text,
             flush=True
